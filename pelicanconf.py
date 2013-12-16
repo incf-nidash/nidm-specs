28ea39387de6fbe586f671db2d2d0ecc949362a0
Nolan Nichols <nolan.nichols@gmail.com>
2013-12-15 20:28:02 -0800

    add: include spec

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'UTC'
 
 DEFAULT_LANG = u'en'
 
-STATIC_PATHS = ['images', 'extra/CNAME']
+STATIC_PATHS = ['images', 'spec', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 # Feed generation is usually not desired when developing
