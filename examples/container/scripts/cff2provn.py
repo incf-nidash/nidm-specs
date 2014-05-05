4bde406d01ca0e8b21183325e84d6771b85ee1b8
Fariba Fana <faribafana@yahoo.com>
2014-05-05 10:47:26 -0700

    Fixed: incorportated Nolan's suggestions including fixing namespaces

--- examples/container/scripts/cff2provn.py
+++ examples/container/scripts/cff2provn.py
@@ -1,34 +1,53 @@
+#!/usr/bin/env python
+
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import urllib2
 import prov.model as prov
 from uuid import uuid1
 
+if __name__ == "__main__":
+  import argparse
+  parser = argparse.ArgumentParser(prog='cff2provn.py', description=__doc__)
+  parser.add_argument('-m', '--meta_file', type=str, required=True,
+                        help='Path to meta.cml file')
+  parser.add_argument('-p', '--project_id', type=str, required=True,
+                        help='Project tag to use for the generated prov files')
+  parser.add_argument('-o', '--output_dir', type=str,
+                        help='Output directory')
+
+  args = parser.parse_args()
+  filename = args.meta_file
+  project_id = args.project_id
+  output_dir = args.output_dir
+  if output_dir is None:
+    output_dir = os.getcwd()
+
 dcterms = prov.Namespace("dcterms", "http://purl.org/dc/terms/")
 xsd = prov.Namespace("xsd","http://www.w3.org/2001/XMLSchema#")
-cff = prov.Namespace("cff", "http://www.connectomics.org/cff-2/")
+cml = prov.Namespace("cml", "http://www.connectomics.org/cff-2/")
+nidm = prov.Namespace("nidm", "http://www.incf.org/ns/nidash/fs#")
 
 # uuid method
 get_id = lambda : uuid1().hex
 
-# parse cff xml file
-filename = "/Users/fariba/Desktop/UCI/freesurfer/scripts/meta.cml"
+# parse cml xml file
+#filename = "/Users/fariba/Desktop/UCI/freesurfer/scripts/meta-MC-SCA-023_tp1.cml"
 tree = xml.dom.minidom.parse(filename)
 collections = tree.documentElement
 
 g = prov.ProvBundle()
 g.add_namespace(xsd)
 g.add_namespace(dcterms);
-g.add_namespace("prov", "http://www.w3.org/ns/prov-o/")
-g.add_namespace(cff)
+g.add_namespace(cml)
 
 
-url_entity = g.entity(cff[get_id()])
-url_entity.add_extra_attributes({prov.PROV['type']: 'cml:connectome',
+url_entity = g.entity(cml[get_id()])
+url_entity.add_extra_attributes({prov.PROV['type']: nidm['nidm:ConnectomeFileFormat'],
   prov.PROV['location']:prov.Literal(filename,prov.XSD['String'])})
 
-cml_collection = g.collection(cff[get_id()])
-cml_collection.add_extra_attributes({prov.PROV['type']: cff['tag_collection'],prov.PROV['label']:filename})
+cml_collection = g.collection(cml[get_id()])
+cml_collection.add_extra_attributes({prov.PROV['type']: cml['ConnectomeFileFormat'], prov.PROV['type']: cml['CMLTagCollection'],prov.PROV['label']:filename})
 g.wasDerivedFrom(cml_collection,url_entity)
 
 # get species, subject_name, and subject_timepoint
@@ -47,8 +66,8 @@ sub = sub.replace('<cml:tag key="subject_name">','').replace('</cml:tag>','')
 tp = tp.replace('<cml:tag key="subject_timepoint">','').replace('</cml:tag>','')
 #print species + " " + sub + " " + tp
 
-cml_meta = g.entity(cff[get_id()]);
-cml_meta.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_meta_tag',cff['species']:species,cff['timepoint']:tp,cff['subject_name']:sub})
+cml_meta = g.entity(cml[get_id()]);
+cml_meta.add_extra_attributes({prov.PROV['type']:cml['ConnectomeMeta'],cml['species']:species,cml['timepoint']:tp,cml['subject_name']:sub})
 g.hadMember(cml_collection, cml_meta)
 
 volumes = collections.getElementsByTagName("cml:connectome-volume")
@@ -57,8 +76,12 @@ for v in volumes:
   c = c + 1
   #print v.getAttribute("src") + " " + v.getAttribute("dtype") + " " + v.getAttribute("name") + " " + v.getAttribute("fileformat")
   #print v.attributes['fileformat'].value
-  cml_volume = g.entity(cff[get_id()])
-  cml_volume.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_volume_tag',cff['dtype']:v.getAttribute("dtype"),cff['src']:v.getAttribute("src"),cff['name']:v.getAttribute("name"),cff['fileformat']:v.getAttribute("fileformat")})
+  dtype = v.getAttribute('dtype');
+  src = v.getAttribute('src')
+  name = v.getAttribute('name')
+  fileformat = v.getAttribute('fileformat')
+  cml_volume = g.entity(cml[get_id()])
+  cml_volume.add_extra_attributes({prov.PROV['type']:cml['connectome-volume'],cml['dtype']:dtype,cml['src']:src,cml['name']:name,cml['fileformat']:fileformat})
   g.hadMember(cml_collection,cml_volume)
 
 tracks = collections.getElementsByTagName("cml:connectome-track")
@@ -66,8 +89,12 @@ c = 0
 for t in tracks:
   c = c + 1
   #print t.getAttribute("src") + " " + t.getAttribute("dtype") + " " + t.getAttribute("name") + " " + t.getAttribute("fileformat")
-  cml_track = g.entity(cff[get_id()])
-  cml_track.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_track_tag',cff['dtype']:t.getAttribute("dtype"),cff['src']:t.getAttribute("src"),cff['name']:t.getAttribute("name"),cff['fileformat']:t.getAttribute("fileformat")})
+  dtype = t.getAttribute('dtype');
+  src = t.getAttribute('src')
+  name = t.getAttribute('name')
+  fileformat = t.getAttribute('fileformat')
+  cml_track = g.entity(cml[get_id()])
+  cml_track.add_extra_attributes({prov.PROV['type']:cml['connectome-track'],cml['dtype']:dtype,cml['src']:src,cml['name']:name,cml['fileformat']:fileformat})
   g.hadMember(cml_collection,cml_track)
 
 networks = collections.getElementsByTagName("cml:connectome-network")
@@ -75,8 +102,12 @@ c = 0
 for n in networks:
   c = c + 1
   #print n.getAttribute("src") + " " + n.getAttribute("dtype") + " " + n.getAttribute("name") + " " + n.getAttribute("fileformat")
-  cml_network = g.entity(cff[get_id()])
-  cml_network.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_network_tag',cff['dtype']:n.getAttribute("dtype"),cff['src']:n.getAttribute("src"),cff['name']:n.getAttribute("name"),cff['fileformat']:n.getAttribute("fileformat")})
+  dtype = n.getAttribute('dtype');
+  src = n.getAttribute('src')
+  name = n.getAttribute('name')
+  fileformat = n.getAttribute('fileformat')
+  cml_network = g.entity(cml[get_id()])
+  cml_network.add_extra_attributes({prov.PROV['type']:cml['connectome-network'],cml['dtype']:dtype,cml['src']:src,cml['name']:name,cml['fileformat']:fileformat})
   g.hadMember(cml_collection,cml_network)
 
 surfaces = collections.getElementsByTagName("cml:connectome-surface")
@@ -84,8 +115,12 @@ c = 0
 for s in surfaces:
   c = c + 1
   #print s.getAttribute("src") + " " + s.getAttribute("dtype") + " " + s.getAttribute("name") + " " + s.getAttribute("fileformat")
-  cml_surface = g.entity(cff[get_id()])
-  cml_surface.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_surface_tag',cff['dtype']:s.getAttribute("dtype"),cff['src']:s.getAttribute("src"),cff['name']:s.getAttribute("name"),cff['fileformat']:s.getAttribute("fileformat")})
+  dtype = s.getAttribute('dtype');
+  src = s.getAttribute('src')
+  name = s.getAttribute('name')
+  fileformat = s.getAttribute('fileformat')
+  cml_surface = g.entity(cml[get_id()])
+  cml_surface.add_extra_attributes({prov.PROV['type']:cml['connectome-surface'],cml['dtype']:dtype,cml['src']:src,cml['name']:name,cml['fileformat']:fileformat})
   g.hadMember(cml_collection,cml_surface)
 
 data = collections.getElementsByTagName("cml:connectome-data")
@@ -93,15 +128,30 @@ c = 0
 for d in data:
   c = c + 1
   #print d.getAttribute("src") + " " + d.getAttribute("dtype") + " " + d.getAttribute("name") + " " + d.getAttribute("fileformat")
-  cml_data = g.entity(cff[get_id()])
-  cml_data.add_extra_attributes({prov.PROV['type']:'cff:cml_tag',prov.PROV['label']:'cml_data_tag',cff['dtype']:d.getAttribute("dtype"),cff['src']:d.getAttribute("src"),cff['name']:d.getAttribute("name"),cff['fileformat']:d.getAttribute("fileformat")})
+  dtype = d.getAttribute('dtype');
+  src = d.getAttribute('src')
+  name = d.getAttribute('name')
+  cml_data = g.entity(cml[get_id()])
+  cml_data.add_extra_attributes({prov.PROV['type']:cml['connectome-data'],cml['dtype']:dtype,cml['src']:src,cml['name']:name,cml['fileformat']:fileformat})
   g.hadMember(cml_collection,cml_data)
 
-
 provn = g.get_provn()
-with open('cff-sample.provn', 'wt') as fp:
+outfilename = output_dir + '/'+project_id
+with open(outfilename+'.provn', 'wt') as fp:
   fp.writelines(provn)
 
-g.rdf().serialize('cff-sample.ttl', format='turtle')
-g.rdf().serialize('cff-sample.xml', format='xml')
-exit()
+g.rdf().serialize(outfilename+'.ttl', format='turtle')
+g.rdf().serialize(outfilename+'.xml', format='xml')
+
+
+'''
+if __name__ == "__main__":
+  import argparse
+  parser = argparse.ArgumentParser(prog='cff2provn.py', description=__doc__)
+  parser.add_argument('-m', '--meta_file', type=str, required=True,
+                        help='Path to meta.cml file')
+
+  args = parser.parse_args()
+  filename = args.meta_file
+  print filename
+'''
