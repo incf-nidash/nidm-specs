5c234f7557ad67dc169d14b0c1b2d37454d41cb1
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-06-19 16:12:04 +0100

    dimensions -> dimensionsInVoxels

--- test/groundTruthScripts/coordinate_space_entity.m
+++ test/groundTruthScripts/coordinate_space_entity.m
@@ -15,8 +15,8 @@ function coordinate_space_entity(niftiFile, num, forcedelete)
     niftiImg = nifti(niftiFile);
     niftiHdr = niftiImg.hdr;
     
-    dimensions = [ '[' num2str(vol.dim, '%g, ')];
-    dimensions(end) = ']';
+    dimensionsInVoxels = [ '[' num2str(vol.dim, '%g, ')];
+    dimensionsInVoxels(end) = ']';
     
     voxelSize = [ '[' num2str(niftiHdr.pixdim(2:4), '%g, ')];
     voxelSize(end) = ']';
@@ -49,7 +49,7 @@ function coordinate_space_entity(niftiFile, num, forcedelete)
     entities = [entities '\n\t' 'nidm:voxelSize = "' voxelSize '" %%%% xsd:string,'];
     entities = [entities '\n\t' 'nidm:worldCoordinateSystem = ''nidm:mniCoordinateSystem'','];
     entities = [entities '\n\t' 'nidm:numberOfDimensions = "' num2str(numel(vol.dim)) '" %%%% xsd:int,'];
-    entities = [entities '\n\t' 'nidm:dimensions = "' dimensions '" %%%% xsd:string])'];
+    entities = [entities '\n\t' 'nidm:dimensionsInVoxels = "' dimensionsInVoxels '" %%%% xsd:string])'];
 
     fprintf(myfile, entities);
     fclose(myfile);
