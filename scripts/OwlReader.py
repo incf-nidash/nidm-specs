f40378c985de119dd96b12e255f2ba187637f881
cmaumet <c.m.j.maumet@warwick.ac.uk>
2015-03-20 16:54:42 +0000

    refactoring: transitive_subjects to find children

--- scripts/OwlReader.py
+++ scripts/OwlReader.py
@@ -22,10 +22,7 @@ class OwlReader():
         self.classes = self.get_class_names() 
         self.properties = self.get_property_names()
         # For each class find out attribute list as defined by domain in attributes
-        attributes_ranges = self.get_attributes()
-        self.attributes = attributes_ranges[0]
-        self.ranges = attributes_ranges[1]      
-        self.type_restrictions = attributes_ranges[2]     
+        self.attributes, self.ranges,  self.type_restrictions = self.get_attributes()
 
 
     def get_class_names(self):
@@ -146,41 +143,12 @@ class OwlReader():
                         for sub_range_name in self.graph.objects(range_name, OWL['onDatatype']):
                             range_name = sub_range_name
 
-                    if data_property in ranges:
-                        ranges[data_property].add(range_name)
-                    else:
-                        ranges[data_property] = set([range_name])
-                    # FIXME: more elegant?
+
+                    ranges.setdefault(data_property, set()).add(range_name)
+
                     # Add child_class to range (for ObjectProperty)
-                    for child_class in self.graph.subjects(RDFS['subClassOf'], range_name):
-                        # Add range to current class
-                        if data_property in ranges:
-                            ranges[data_property].add(child_class)
-                        else:
-                            ranges[data_property] = set([child_class])
-                        range_name = child_class
-                        for child_class in self.graph.subjects(RDFS['subClassOf'], range_name):
-                            # Add attribute to current class
-                            if data_property in ranges:
-                                ranges[data_property].add(child_class)
-                            else:
-                                ranges[data_property] = set([child_class])
-                            range_name = child_class
-                            for child_class in self.graph.subjects(RDFS['subClassOf'], range_name):
-                                # Add attribute to current class
-                                if data_property in ranges:
-                                    ranges[data_property].add(child_class)
-                                else:
-                                    ranges[data_property] = set([child_class])
-                                range_name = child_class
-                                for child_class in self.graph.subjects(RDFS['subClassOf'], range_name):
-                                    # Add attribute to current class
-                                    if data_property in ranges:
-                                        ranges[data_property].add(child_class)
-                                    else:
-                                        ranges[data_property] = set([child_class])
-                                    range_name = child_class
-                
+                    for child in self.graph.transitive_subjects(RDFS['subClassOf'], range_name):
+                        ranges.setdefault(data_property, set()).add(child)
                     
 
         return list((attributes, ranges, restrictions))
