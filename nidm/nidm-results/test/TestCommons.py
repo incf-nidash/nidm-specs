cmaumet/ni-dm

New SPM example for non-parametric stat: example 005

nidm/nidm-results/test/TestCommons.py
@@ -27,6 +27,7 @@ example_filenames = import_test_filenames.union(set([
                                 os.path.join('spm', 'example002', 'spm_results_2contrasts.provn'),
                                 os.path.join('spm', 'example003', 'spm_results_conjunction.provn'),
                                 os.path.join('spm', 'example004', 'spm_inference_activities.provn'),
+                                os.path.join('spm', 'example005', 'spm_nonparametric.provn'),
                                 os.path.join('fsl', 'fsl_results.provn')
                             ]))
 

incf-nidash/nidm-specs

nidm/nidm-results/test/TestCommons.py
@@ -32,6 +32,7 @@ example_filenames = import_test_filenames.union(set([
                                 os.path.join('spm', 'example003', 'spm_results_conjunction.provn'),
                                 os.path.join('spm', 'example004', 'spm_inference_activities.provn'),
                                 os.path.join('spm', 'example005', 'nidm.provn'),
+                                os.path.join('spm', 'nonparam', 'spm_nonparametric.provn'),
                                 os.path.join('fsl', 'fsl_results.provn')
                             ]))
 