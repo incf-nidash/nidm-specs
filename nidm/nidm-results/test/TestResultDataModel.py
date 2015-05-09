157b2829ed6f324a6f2139ae868c3580b76934e5
cmaumet <c.m.j.maumet@warwick.ac.uk>
2015-05-09 12:23:49 +0100

    Tolerance when comparing floats

--- nidm/nidm-results/test/TestResultDataModel.py
+++ nidm/nidm-results/test/TestResultDataModel.py
@@ -9,6 +9,7 @@ import os
 import sys
 import rdflib
 import re
+import numpy as np
 
 import logging
 
@@ -289,7 +290,16 @@ class TestResultDataModel(object):
                                 if o.replace(" ", "") == o_gt.replace(" ", ""):
                                     same_json_array = True
 
-                        if not same_json_array:
+                        # If literal is a float allow for a small tolerance to
+                        # deal with possibly different roundings
+                        close_float = False
+                        if o.datatype == XSD.float:
+                            for o_gt in gt_graph.objects(s,  p):
+                                if o_gt.datatype == XSD.float:
+                                    close_float = np.isclose(
+                                        o.value, o_gt.value)
+
+                        if not same_json_array and not close_float:
                             exc_wrong_literal += \
                                 "\nWrong o:\t %s should be %s?" \
                                 "\n\t\t ... in '%s %s %s'" \
