3be13fa7e999e9c0b780973915fe6a079d54a3cb
Karl Helmer <helmer@nmr.mgh.harvard.edu>
2020-04-19 10:24:58 -0400

    updated dicom graph

--- nidm/nidm-experiment/scripts/create_expe_specification.py
+++ nidm/nidm-experiment/scripts/create_expe_specification.py
@@ -127,8 +127,10 @@ def main():
     subcomponents["Properties"] = [
         NIDM['dicomTag'], NIDM['bathSolution'],
         NIDM['cellType'], NIDM['channelNumber'], NIDM['electrodeImpedance'], NIDM['groupLabel'], 
-        NIDM['hollowElectrodeSolution'], NIDM['hadImageContrastType'], NIDM['hadImageUsageType'], 
-        NIDM['numberOfChannels'], NIDM['appliedFilter'], NIDM['solutionFlowSpeed'], NIDM['recordingLocation']
+        NIDM['hollowElectrodeSolution'], NIDM['hadImageContrastType'], NIDM['hadImageUsageType'], NIDM['hadForProperPart'], 
+        NIDM['hadForScale'], NIDM['hadForVariable'], NIDM['wasMeasuredBy'], NIDM['hadForSubinstrument'], NIDM['wasScaleOf'], 
+        NIDM['hadNumericalValue'], NIDM['numberOfChannels'], NIDM['appliedFilter'], NIDM['solutionFlowSpeed'], 
+        NIDM['recordingLocation']
     ]
 
     # Add manually used and wasDerivedFrom because these are not stored in the
